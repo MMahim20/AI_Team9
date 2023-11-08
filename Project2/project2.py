3ec6c25b30319a1901a87cb57d9450f03c5d5ad7
mmahim20 <mahimmathur20@gmail.com>
2023-11-07 20:45:03 -0600

    better skeleton code. Prep inputs and outputs

--- Project2/project2.py
+++ Project2/project2.py
@@ -9,48 +9,62 @@ import pandas as pd
 df = pd.read_csv("train.csv")
 print(df)
 
-
 # Create X and y matrices
 
 # create the y label vector = result (Survived) column.
-y = df['Survived']
+y = df['target']
 
 # use the rest of the dataframe for matrix
-X = df.drop(['Survived', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked', 'Age', 'Fare'], axis=1)
+X = df['text']
 
+# explore the data
 print('y:')
 print(y)
 
 print('\n--------------------')
+
 print('\nX:')
 print(X)
 
+############## TRAINING AND VALIDATION TESTING
 # Split training data into training and validation set
 
 # 20% testing, 80% training
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Build random forest model
+# Load pre-trained embedding model
+###CHANGE
 clf = RandomForestClassifier(max_depth=2, random_state=0)
 
 # Train model with training data
+###CHANGE
 clf.fit(X_train, y_train)
 
 # Use validation data to test accuracy during training
-y_pred = clf.predict(X_test)
+###CHANGE
+y_pred = clf.predict(X_val)
 
-accuracy = accuracy_score(y_test, y_pred)
+accuracy = accuracy_score(y_val, y_pred)
 print("Training Accuracy: ", accuracy)
 
+# Fine-tune pre-trained model on tweets
+
+
+############## VISUALIZATIONS
+# Create visualizations showing prediction outcomes on training data
+
+
+############## TESTING
 # Use test data set to get final predicted goodness
 test_df = pd.read_csv("test.csv")
 
 # Get feature matrix from test data
-test_df = test_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked', 'Age', 'Fare'], axis=1)
-print(test_df)
+X_test = test_df['text']
+print(X_test)
 
-y_pred = clf.predict(test_df)
+###CHANGE
+y_pred = clf.predict(X_test)
 
 # output submission file
-output = pd.DataFrame({'PassengerId': test_df.PassengerId, 'Survived': y_pred})
+output = pd.DataFrame({'id': test_df.id, 'target': y_pred})
 output.to_csv('submission.csv', index=False)
