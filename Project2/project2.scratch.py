071fa147392fe276e58a4545ef6db8cc60840bf9
mmahim20 <mahimmathur20@gmail.com>
2023-11-09 18:28:35 -0600

    took out old comments/code

--- Project2/project2.scratch.py
+++ Project2/project2.scratch.py
@@ -13,8 +13,6 @@ import matplotlib.pyplot as plt
 
 # adapted from the tutorial: https://www.kaggle.com/code/alexia/kerasnlp-starter-notebook-disaster-tweets
 
-
-
 EPOCHS = 2
 AUTO = tf.data.experimental.AUTOTUNE
 
@@ -22,7 +20,7 @@ import os
 os.environ['KERAS_BACKEND'] = 'tensorflow'
 
 # Import and explore data
-df = pd.read_csv("Project2/train.csv")
+df = pd.read_csv("train.csv")
 print(df)
 NUM_TRAINING_EXAMPLES = df.shape[0]
 BATCH_SIZE = 32
@@ -66,7 +64,6 @@ if os.path.exists(file_path):
 else:
     print("training new model")
     # Load pre-trained embedding model
-    ###CHANGE
     preset= "distil_bert_base_en_uncased"
 
     # Use a shorter sequence length.
@@ -81,9 +78,10 @@ else:
                                                                 num_classes=2)
 
     classifier.summary()
+
     # Train model with training data
     classifier.compile(
-        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True), #'binary_crossentropy',
+        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         optimizer=keras.optimizers.Adam(learning_rate=1e-5),
         metrics= ["accuracy"]  
     )
@@ -113,8 +111,6 @@ def displayConfusionMatrix(y_true, y_pred, dataset):
     disp.ax_.set_title("Confusion Matrix on " + dataset + " Dataset -- F1 Score: " + str(f1_score.round(2)))
 
 
-
-
 # Use validation data to test accuracy during training
 y_pred = classifier.predict(X_train)
 
@@ -124,33 +120,20 @@ print("y_pred shape:", y_pred.shape)
 print("y_test values:", y_val)
 print("y_pred values:", y_pred)
 
-displayConfusionMatrix(y_train, y_pred, "Training")
-# accuracy = accuracy_score(y_test, y_pred)
-# print("Training Accuracy: ", accuracy)
-
-# Fine-tune pre-trained model on tweets
-
 
 ############## VISUALIZATIONS
 # Create visualizations showing prediction outcomes on training data
+displayConfusionMatrix(y_train, y_pred, "Training")
 
 
 ############## TESTING
-# # Use test data set to get final predicted goodness
-test_df = pd.read_csv("Project2/test.csv")
+# Use test data set to get final predicted goodness
+test_df = pd.read_csv("test.csv")
 
-# # Get feature matrix from test data
+# Get feature matrix from test data
 X_test = test_df['text']
-# print(X_test)
 
-# ###CHANGE
-# y_pred = classifier.predict(X_test)
-
-# # output submission file
-# ### CHANGE based on y_pred
-# # output = pd.DataFrame({'id': test_df.id, 'target': y_pred})
-# # output.to_csv('submission.csv', index=False)
-
+# output submission file
 print("\n generating submission\n")
 # print(np.argmax(classifier.predict(X_test), axis=1))
 
