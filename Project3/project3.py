169472d6dc29e2c976e86f3b6c98cd5b2e069b23
Mahim Mathur <mahimmathur20@gmail.com>
2023-11-20 18:36:57 -0600

    set up the data for the CNN model to use

--- Project3/project3.py
+++ Project3/project3.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 
-from keras.utils.np_utils import to_categorical
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
 from keras.optimizers import RMSprop
@@ -24,11 +25,23 @@ y = df['label']
 # use the rest of the dataframe for matrix
 X = df.drop(['label'], axis=1)
 
+# Normalize the data to be on [0..1] scale
+X = X / 255.0
+
+# Reshape the data to be an image in 3 dimensions 28pixels x 28pixels x 1 channel for the Keras CNN
+X = X.values.reshape(-1,28,28,1)
+
+# encode y label vector to be one hot vectors. There are 10 digits (0-9).
+y = to_categorical(y, num_classes=10)
+
 ############## TRAINING AND VALIDATION TESTING
 # Split training data into training and validation set
-
 # 20% testing, 80% training
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# show example
+example = plt.imshow(X_train[0][:,:,0])
+plt.show()
 
 ############## SET UP THE NETWORK
 # Create CNN network
@@ -42,5 +55,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 ############## TESTING
 # Use test data set to get final predicted goodness
 test_df = pd.read_csv("test.csv")
+
+# Normalize the data to be on [0..1] scale
+X_test = test_df / 255.0
+
+# Reshape the data to be an image in 3 dimensions 28pixels x 28pixels x 1 channel for the Keras CNN
+X_test = X_test.values.reshape(-1,28,28,1)
+
 # X_test = test_df['text']
 # print(X_test)
